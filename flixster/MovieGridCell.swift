ae2223611663f45c0ba79ae6da021788b2b7922d
Anthony Gordon <anthony.fgordon64@gmail.com>
2021-02-21 19:43:57 -0500

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Anthony Gordon on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
